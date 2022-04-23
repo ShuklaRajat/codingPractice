492c382761a660b06e708f2bbd98ac006c2fd06a
Rajat Shukla <rajats.think@gmail.com>
2022-04-24 00:05:53 +0530

    Day 4 of 30DayCodeChallenge
    Fixed a couple of more test cases for the problem on leetcode. More test cases failing. Will work on the logic on Day 5.

--- javaCodingPractice/src/LoveBabbar/mergeIntervals.java
+++ javaCodingPractice/src/LoveBabbar/mergeIntervals.java
@@ -1,68 +1,37 @@
 package LoveBabbar;
 
 public class mergeIntervals {
+    public static int[][] removeElement(int[][] intervals, int index){
+        int[][] removedElementArr = new int[intervals.length-1][2];
 
-//    public static int[][] removeElement(int[][] intervals, int index){
-////        int i;
-////        for (i = index; i<intervals.length-index; i++){
-////            intervals[i] = intervals[i+1];
-////        }
-////        intervals[i] = null;
-//
-////        System.out.println("-----------------------");
-////        return intervals;
-////
-//        int[][] removedElementArr = new int[intervals.length-1][2];
-//
-//        for(int i=0, k=0; i< intervals.length; i++){
-//           if(i==index){
-//               continue;
-//           } else {
-//               removedElementArr[k] = intervals[i];
-//               k++;
-//           }
-//        }
-//
-//        System.out.println("After removing : ");
-//        for(int i=0; i<removedElementArr.length; i++){
-//            System.out.println(removedElementArr[i][0] + " , " + removedElementArr[i][1]);
-//        }
-//        System.out.println("-------------------------------");
-//
-//        return removedElementArr;
-//    }
-//public static int[][] merge(int[][] intervals) {
-//
-//    for(int i=0; i<intervals.length-1; i++){
-//        System.out.println("i:"+i+"\nintervals[][]="+intervals);
-//        if(intervals[i][1] > intervals[i+1][0]){
-//            System.out.println("Merging "+ intervals[i] + " and "+ intervals[i+1] );
-//            intervals[i] = new int[]{intervals[i][0], intervals[i + 1][1]};
-//            intervals = removeElement(intervals, i+1);
-//            System.out.println("Merged intervals = "+intervals);
-////                i--;
-//        }
-//    }
-//
-//    return intervals;
-//}
-
-
-public static int[][] removeElement(int[][] intervals, int index){
-    int[][] removedElementArr = new int[intervals.length-1][2];
-
-    for(int i=0, k=0; i< intervals.length; i++){
-        if(i==index){
-            continue;
-        } else {
-            removedElementArr[k] = intervals[i];
-            k++;
+        for(int i=0, k=0; i< intervals.length; i++){
+            if(i==index){
+                continue;
+            } else {
+                removedElementArr[k] = intervals[i];
+                k++;
+            }
         }
+
+        return removedElementArr;
     }
 
-    return removedElementArr;
-}
+    public static int[][] sortArr(int[][] arr) {
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                int tmp[];
+                if (arr[i][0] > arr[j][0]) {
+                    tmp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = tmp;
+                }
+            }
+        }
+        return arr;
+    }
     public static int[][] merge(int[][] intervals) {
+
         for(int i=0; i<intervals.length-1; i++){
             if(intervals[i][1] >= intervals[i+1][0]){
                 if(intervals[i][0] < intervals[i+1][0]){
@@ -89,9 +58,10 @@ public static int[][] removeElement(int[][] intervals, int index){
 
 
     public static void main(String[] args) {
-        int[][] intervals = {{1,3},{2,6},{8,10},{15,18}};
-
-        intervals = merge(intervals);
+        int[][] intervals
+//                = {{1,3},{2,6},{8,10},{15,18}};
+                        ={{1,4},{0,0}};
+        intervals = merge(sortArr(intervals));
 
         for(int i=0; i<intervals.length; i++){
             System.out.println(intervals[i][0] + " , " + intervals[i][1]);
